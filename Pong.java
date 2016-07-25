a8c371cb58a0a9cb2fb4b6a65132220ee80651f1
Steven Degutis <sbdegutis@gmail.com>
2016-07-25 17:53:59 -0600

    applets are better

--- Pong.java
+++ Pong.java
@@ -1,61 +1,74 @@
 package com.pong;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import java.awt.Shape;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
 
-import javax.swing.JComponent;
-import javax.swing.JFrame;
+import java.applet.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.geom.*;
 
-public class Pong extends JFrame
+public class Pong extends JApplet
 {
-	Moveable paddle1 = new Moveable(100);
-	Moveable paddle2 = new Moveable(900);
+	private PaintSurface canvas;
 	
-	Moveable ball = new Moveable(450);
-
-	public static void main(String[] args)
+	public static void main()
+	
+	public void init()
 	{
-		new Pong();
-	}
+		this.setSize(1100, 1000);	
+		canvas = new PaintSurface();
+		this.add(canvas, BorderLayout.CENTER);
 	
-	public Pong()
-	{
-		this.setSize(1100, 1000);
-		this.setTitle("Pong");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
-		this.add(new Paint(), BorderLayout.CENTER);
-	
-		this.setVisible(true);
+		Timer t = new Timer(20, e -> {canvas.repaint();} );t.start();	
 	}
 
-	private class Paint extends JComponent
+	class PaintSurface extends JComponent
 	{
+		boolean directionIsRight = true;
+		boolean directionIsUp = true;
+		int speed = 4;
+		int paddle1Y = 250;
+		int paddle2Y = 250;
+		int ballX = 450;
+		int ballY = 250;
+		
 		public void paint(Graphics g) 
 		{
 			Graphics2D g2D = (Graphics2D)g;
-			
 			g2D.setRenderingHint
 			(
 					RenderingHints.KEY_ANTIALIASING, 
 					RenderingHints.VALUE_ANTIALIAS_ON
 			);
+					
+			Shape paddle1Image = new Rectangle2D.Float(100, paddle1Y, 20, 150);
+			Shape paddle2Image = new Rectangle2D.Float(900, paddle2Y, 20, 150);
+			Shape ballImage = new Ellipse2D.Float(ballX, ballY, 100, 100);
 		
-			g2D.setPaint(Color.BLACK);
+			ballX += speed;
 			
-			Shape paddle1Image = new Rectangle2D.Float(paddle1.x, paddle1.y, 20, 150);
-			Shape paddle2Image = new Rectangle2D.Float(paddle2.x, paddle2.y, 20, 150);
-			Shape ballImage = new Ellipse2D.Float(100, 250, 100, 100);
-
-			g2D.draw(paddle1Image);
-			g2D.draw(paddle2Image);
-			g2D.draw(ballImage);
+			g2D.setPaint(Color.BLACK);
+			g2D.fill(paddle1Image);
+			g2D.fill(paddle2Image);
+			g2D.fill(ballImage);
 		}
 	}
+
+//	void ketPressed(KeyEvent key)
+//	{
+//		
+		
+		
+		
+		
+		
+		
+//		if(key.getSource() == "w")
+//		{
+//			paddle1Y++;
+//		}
+	
+	
+	
+	
+//	}
 }
